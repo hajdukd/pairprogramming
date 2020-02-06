cf0e09836da2341039dd4ee79d36b69e0879ccf4
Daniel Hajduk <daniel.piotr.hajduk@gmail.com>
2020-02-06 12:17:08 +0100

    feat: floobits

---
+++ src/com/ttom/test/HelloWorld.groovy
@@ -0,0 +1,11 @@
+package com.ttom.test
+/**
+ * @author Daniel Hajduk
+ * Created 06/02/2020
+ */
+class HelloWorld {
+
+    static void main(String[] args) {
+        print("HELLO WORLD") dsadsad
+    }
+}
